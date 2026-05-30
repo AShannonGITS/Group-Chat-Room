32001c9fe3af04b8b272e6db2f10911cf9d912cd
Adam <dadsmainman2006@yahoo.com>
2026-05-30 16:26:51 -0700

    Imported JSON & fixed bugs

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from mainclass import chat_room
 
 import time
 
-exit()
-
 def main():
     Valid = True
     
@@ -17,7 +15,7 @@ def main():
         UserMenuInput = input("Please pick an option from the menu: ").lower()
         
         if UserMenuInput == "start" or UserMenuInput == "s":
-            user_input = input("Would you like to start the client or server? ")
+            user_input = input("Would you like to start the client or server? ").lower()
             
             if user_input == "client" or user_input == "c":
                 main_client()
@@ -34,17 +32,17 @@ def main():
             options_menu()
             
             
-            OptionsMenuInput = input("Please pick from one of the options: ")
+            OptionsMenuInput = input("Please pick from one of the options: ").lower()
             
             if OptionsMenuInput == "username" or OptionsMenuInput == "u":
                 print("")
             elif OptionsMenuInput == "server" or OptionsMenuInput == "server settings" or OptionsMenuInput == "s":
                 
                 chat_room.server_ip = input("Input the new IP Addres you would like the server to be hosted on (Local only): ")
-                chat_room.server_port = (input("Input the new port number for the server: "))
+                chat_room.server_port = int(input("Input the new port number for the server: "))
                 
             elif OptionsMenuInput == "client" or OptionsMenuInput == "client settings" or OptionsMenuInput == "c":
-                client_input = input("")
+                client_input = input("").lower()
                 
                 if client_input == "" or client_input == "":
                     print("")
--- mainfunctions.py
+++ mainfunctions.py
@@ -6,6 +6,7 @@ from mainclass import chat_room
 
 import socket
 import time
+import json
 
 def main_instructions():
     print("This program is a chat room that multiple clients can connect to.")
@@ -17,41 +18,35 @@ def program_instructions():
 def options_menu():
     print("Options Test 1")
     
-def chat_log(message, author):
+def write_chat_log(message, author):
     with open("chat_log.txt", "a") as log:
         log.write(f"    By:{author} \n")
         log.write(f"{message} \n")
         
 def chat_log_search():
-    moderator_input = input("Which message would you like to find? ")
+    moderator_input = input("Which message would you like to find? ").lower()
     
     with open("chat_log.txt", "r") as log_search:
         log_search.read(moderator_input)
         #WIP!!!
         
 def write_user_info(user_info_input):
-    with open("user_info.txt", "a") as w_user_info:
-        w_user_info.write(f"{user_info_input} \n")
-        #WIP!!!
+    # Use JSON
         
 def read_user_info(user):
-    with open("user_info.txt", "r") as r_user_info:
-        r_user_info.read(user)
+    # Use JSON
         
 def write_group_info(user_group_input):
-    with open("user_info.txt", "a") as w_user_info:
-        w_user_info.write(f"{user_group_input} \n")
-        #WIP!!!
+    # Use JSON
         
 def read_group_info(group):
-    with open("user_info.txt", "r") as r_user_info:
-        r_user_info.read(group)
+    # Use JSON
         
 def server_console(console_input_valid):
     while console_input_valid == True:
-            server_input = input("")
+            server_input = input("").lower()
             
-            if server_input == "moderate" or server_input == "":
+            if server_input == "moderation tools" or server_input == "m":
                 print("1")
             elif server_input == "" or server_input == "":
                 print("2")
